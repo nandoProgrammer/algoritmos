614a59118428b598eb8835ef6329de1680da097b
Fernando Costa <62177135+nandoProgrammer@users.noreply.github.com>
2023-02-07 08:20:17 -0300

    Update buscabinaria.js

--- buscabinaria.js
+++ buscabinaria.js
@@ -1,9 +1,8 @@
 //Algoritmo
 	var binarySearch = function(array, numberToFind){
-
 		var min = 0;
 		var max = array.length - 1;
-        var guess;
+                var guess;
 		
 		var i = 0;
 
@@ -13,21 +12,16 @@
 			guess = Math.floor((max + min) / 2);
 
 			if(array[guess] === numberToFind){
-              
-                console.log('Número encontrado com '+ i +' tentativas');
-                return guess;
-
+			     console.log('Número encontrado com '+ i +' tentativas');
+			     return guess;
 			}
 
 			else if(array[guess] < numberToFind){
-
-				min = guess + 1;
-
+			     min = guess + 1;
 			}
 
 			else{
-
-				max = guess - 1;
+			     max = guess - 1;
 			}
 
 		}
@@ -36,12 +30,11 @@
 	}
 
   //Fim do algoritmo
-
     
-   /*Implementação*/
+  /*Implementação*/
 
   //variável que contem o array a ser busacdo o valor
-	var arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+  var arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
     
  //Chamando a função com os parâmetros correspondentes
  //Primeiro parâmetro: faz referência ao array onde a busca ocorrerá
